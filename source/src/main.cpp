d194e8dba8883914e5398f6842f56c6f4bc88e55
SUPERNOOB20 <andycia802@gmail.com>
2025-08-27 20:43:09 -0300

    a

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -53,7 +53,7 @@ int main()
 {
     bn::core::init();
 
-    global_data global_instance = {
+    struct global_data global_instance = {
 
         // vvv   Do these need top_left() method...?   vvv
 
@@ -71,74 +71,7 @@ int main()
     while(true)
     {
 
-<<<<<<< HEAD
-
-        // "collision" should be "(x1, y1, x2, y2)"
-        // I need to try to make a vector with "spike" collisions, and a vector with "block" collissions.
-
-        // We need to set up a vector of sprite_ptr to represent individual letters.
-        // We can only have a max of 16 letters in this vector.
-        // bn::vector<bn::sprite_ptr, 16> text_sprites;
-
-        
-        // The first two values represent X and Y.
-        // The third is your constant text.
-        // The fourth is your sprite_ptr vector.
-        // text_generator.generate(-6 * 16, -68, "ded", text_sprites);
-
-
-
-
-        change_stage();
-
-        bn::sprite_ptr reimu_sprite = bn::sprite_items::reimu_idle_spritesheet.create_sprite(player_x, player_y);
-
-        if ((frame_counter % (reimu_anim_frames * 3)) < (reimu_anim_frames)) {
-            reimu_sprite.set_tiles(bn::sprite_items::reimu_idle_spritesheet.tiles_item().create_tiles(0));                
-        } else if ((frame_counter % (reimu_anim_frames * 3)) < (reimu_anim_frames * 2)) {
-            reimu_sprite.set_tiles(bn::sprite_items::reimu_idle_spritesheet.tiles_item().create_tiles(1));                
-        } else {
-            reimu_sprite.set_tiles(bn::sprite_items::reimu_idle_spritesheet.tiles_item().create_tiles(2));                
-        }
-
-
-        
-
-        // void akyuu_anim(int frame_counter)    
-        bn::sprite_ptr akyuu_sprite = bn::sprite_items::akyuu_spritesheet.create_sprite(96, 16);
-        akyuu_sprite.set_visible(false);
-
-        if (current_stage == 2){
-
-            akyuu_sprite.set_visible(true);
-
-            if ((frame_counter % (akyuu_anim_frames * 2)) < (akyuu_anim_frames)) {
-                akyuu_sprite.set_tiles(bn::sprite_items::akyuu_spritesheet.tiles_item().create_tiles(0));  
-                BN_LOG("huhh");
-            } else {
-                akyuu_sprite.set_tiles(bn::sprite_items::akyuu_spritesheet.tiles_item().create_tiles(1));                
-            }
-        }
-
-
-
-
-
-        auto current_stage_bg = bn::regular_bg_items::screen1n.create_bg(8, 48);
-
-        if (current_stage == 1) {
-            current_stage_bg = bn::regular_bg_items::screen1n.create_bg(8, 48);
-        } else if (current_stage == 2) {
-            current_stage_bg = bn::regular_bg_items::screen2n.create_bg(8, 48);
-        } else {
-            int nop = 0;            // hey_compiler_whats_up_xddd
-        }
-
-        bn::regular_bg_ptr regular_bg = current_stage_bg;
-
-=======
         rra::game::handle_frame(global_ptr, frame_counter);
->>>>>>> 5306bc5 (Revert "qwq")
 
         frame_counter++;
 
