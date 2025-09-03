74c1bf53e544211d353676c74c5f4cd47b67960a
SUPERNOOB20 <andycia802@gmail.com>
2025-09-03 03:27:18 -0300

    :3

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -30,22 +30,21 @@
 #include "game.h"
 
 
-// #include <vector>
-// #include "bn_vector.h"
+
+
 
 #include <global_resources.h>
 
-/*
+
+// #include <vector>
+#include "bn_vector.h"
 template<typename Type, int MaxSize>
 
-struct global_resources {
+#include <global_resources.h>
 
-        bn::vector<bn::sprite_ptr, 27>        global_player_sprites;        // Idling, jumping, dashing should be available in all levels!
-        bn::vector<bn::sprite_ptr, 100>       global_sprites;               // 128 is the maximum number of sprites that can fit in the GBA's VRAM. But I'm padding 27 here for the player's sprites. And leaving 1 extra, unused slot just in case :P
-        bn::vector<bn::regular_bg_ptr, 4>     global_backgrounds;           // 4 is the maximum number of backgrounds that can fit in the GBA's VRAM.
-
-    };
-*/
+bn::vector<bn::sprite_ptr, 27>        global_player_sprites;        
+bn::vector<bn::sprite_ptr, 100>       global_sprites;               
+bn::vector<bn::regular_bg_ptr, 4>     global_backgrounds;           
 
 
 
@@ -57,9 +56,7 @@ int main()
 
     int frame_counter = 0;    // As a timer for animations :3
 
-    VRAM allocate_VRAM(bn::vector<bn::sprite_ptr, 26> global_player_sprites, bn::vector<bn::sprite_ptr, 100> global_sprites, bn::vector<bn::regular_bg_ptr, 4> global_backgrounds){
-        VRAM new_VRAM = {}
-    }
+    global_VRAM VRAM = allocate_VRAM(global_player_sprites, global_sprites, global_backgrounds);
 
     // Play main menu, intro scene... etc.
 
