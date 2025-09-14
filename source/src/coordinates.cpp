430d9ce4130174349e4630c5476f948ae934817c
SUPERNOOB20 <andycia802@gmail.com>
2025-09-14 15:34:38 -0300

    issue fixed!! :D

--- source/src/coordinates.cpp
+++ source/src/coordinates.cpp
@@ -1,10 +1,9 @@
 #include "coordinates.h"
 #include <tuple>
 
-
 // Defines the location of a rectangle.
 // Useful for hitboxes, collisions, etc.
-struct Coordinates::Coordinates(std::tuple<int, int> top_left_corner, std::tuple<int, int> bottom_right_corner){
+Coordinates::Coordinates(std::tuple<int, int> top_left_corner, std::tuple<int, int> bottom_right_corner)
     : top_left_corner{ top_left_corner }
     , bottom_right_corner { bottom_right_corner }
 {
