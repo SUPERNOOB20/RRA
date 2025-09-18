39c90fa85fd26d5e7dd60ef14a54ff6fd781eb4a
SUPERNOOB20 <andycia802@gmail.com>
2025-09-18 20:52:44 -0300

    Well, I'm calling it a day, I'll taking a break to play some Golden Sun now. And yes, it was absolutely necessary and professional to include that information in the commit message (ha)

--- source/include/player_bounding_boxes.h
+++ source/include/player_bounding_boxes.h
@@ -8,11 +8,40 @@
 
 
 
-class Player {
+class Player
+{
 
-    // I didn't want to duplicate code from "player.h", but alas... *sigh*
+public:
 
-    private:
+    class Damage_Hitbox
+    {
+        private:
+            std::array<int, 2>                 GetPosition_topleft_corner(Player& p) { return {p.player_x + 8, p.player_y + 15}; }
+            std::array<int, 2>                 GetPosition_bottomright_corner(Player& p) { return {p.player_x + 19, p.player_y + 32}; }
+            std::array<std::array<int, 2>, 2>  GetPosition_topleft_and_bottomright_corners(Player& p) { return {{p.player_x + 8, p.player_y + 15}, {p.player_x + 19, p.player_y + 32}}; }
+
+        public:
+            std::tuple<int, int> player_topleft_damage_hitbox();
+            std::tuple<int, int> player_bottomright_damage_hitbox();
+            std::tuple<std::tuple<int, int>, std::tuple<int, int>> damage_hitbox();
+    };
+
+    class Collision_Hitbox
+    {
+        private:
+            std::array<int, 2>                 GetPosition_topleft_corner(Player& p) { return {p.player_x + 11, p.player_y + 22}; }    
+            std::array<int, 2>                 GetPosition_bottomright_corner(Player& p) { return {p.player_x + 15, p.player_y + 25}; }    
+            std::array<std::array<int, 2>, 2>  GetPosition_topleft_and_bottomright_corners(Player& p) { return {{p.player_x + 11, p.player_y + 22}, {p.player_x + 15, p.player_y + 25}}; }    
+        
+        public:
+            std::tuple<int, int> player_topleft_collision_hitbox();
+            std::tuple<int, int> player_bottomright_collision_hitbox();
+            std::tuple<std::tuple<int, int>, std::tuple<int, int>> collision_hitbox();
+            std::array<int, 4> collision_hitbox_array();                                        // Same as collision_hitbox, but returns the result in a different format - returns an array of the 4 vertices :3 In this order: top_left_corner_x, top_left_corner_y, bottom_left_corner_x, bottom_left_corner_y.
+            std::array<std::tuple<int, int>, 4> collision_hitbox_vertex_array();                // Same as collision_hitbox, but returns the result in a different format - returns an array of the 4 vertices :3 In this order: top left corner, top right corner, bottom left corner, bottom right corner.
+    };
+    
+private:
 
     int player_x {0};               // Might be changed later on with fixed-point math (signed 20.11f fixed-point)
     int player_y {0};               // Might be changed later on with fixed-point math (signed 20.11f fixed-point)
@@ -20,25 +49,7 @@ class Player {
     std::tuple<int, int> getPosition() const { return std::tuple<int,int> (player_x, player_y); }
     int getPosition_x() const { return player_x; }
     int getPosition_y() const { return player_y; }
-
-    // End of duplicated code. Phew ":3
-
-
-
-    public:
-
-        std::tuple<int, int> player_topleft_damage_hitbox();
-        std::tuple<int, int> player_bottomright_damage_hitbox();
-        std::tuple<std::tuple<int, int>, std::tuple<int, int>> damage_hitbox();
-
-        std::tuple<int, int> player_topleft_collision_hitbox();
-        std::tuple<int, int> player_bottomright_collision_hitbox();
-        std::tuple<std::tuple<int, int>, std::tuple<int, int>> collision_hitbox();
-        std::array<int, 4> collision_hitbox_array();                                        // Same as collision_hitbox, but returns the result in a different format - returns an array of the 4 vertices :3 In this order: top_left_corner_x, top_left_corner_y, bottom_left_corner_x, bottom_left_corner_y.
-        std::array<std::tuple<int, int>, 4> collision_hitbox_vertex_array();                // Same as collision_hitbox, but returns the result in a different format - returns an array of the 4 vertices :3 In this order: top left corner, top right corner, bottom left corner, bottom right corner.
-
-        // vv  Dropped idea to make collision_hitbox argument call constructors... ew  vv
-        // std::array collision_hitbox(string arr)
+    
 };
 
 
--- source/src/player_bounding_boxes.cpp
+++ source/src/player_bounding_boxes.cpp
@@ -13,48 +13,48 @@
 
 
 
-std::tuple<int, int> Player::player_topleft_damage_hitbox(){
-    std::tuple<int, int> player_topleft_hitbox = {Player::getPosition_x() + 11, Player::getPosition_y() + 22};    // edge INCLUDED
+std::tuple<int, int> Player::Damage_Hitbox::player_topleft_damage_hitbox(){
+    std::tuple<int, int> player_topleft_hitbox = {getPosition_x() + 11, getPosition_y() + 22};    // edge INCLUDED
     return player_topleft_hitbox;
 }
 
-std::tuple<int, int> Player::player_bottomright_damage_hitbox(){
+std::tuple<int, int> Player::Damage_Hitbox::player_bottomright_damage_hitbox(){
     std::tuple<int, int> player_topleft_hitbox = {getPosition_x() + 15, getPosition_y() + 25};    // edge INCLUDED
     return player_topleft_hitbox;
 }
 
-std::tuple<std::tuple<int, int>, std::tuple<int, int>> Player::damage_hitbox(){      // Returns (player_topleft_hitbox(), player_bottomright_hitbox())
+std::tuple<std::tuple<int, int>, std::tuple<int, int>> Player::Damage_Hitbox::damage_hitbox(){      // Returns (player_topleft_hitbox(), player_bottomright_hitbox())
     std::tuple<std::tuple<int, int>, std::tuple<int, int>> hitbox = {player_topleft_damage_hitbox(), player_bottomright_damage_hitbox()};
     return hitbox;
-}
+} 
 
-std::tuple<int, int> Player::player_topleft_collision_hitbox(){
+std::tuple<int, int> Player::Collision_Hitbox::player_topleft_collision_hitbox(){
     std::tuple<int, int> player_topleft_hitbox = {getPosition_x() + 8, getPosition_y() + 15};    // edge INCLUDED
     return player_topleft_hitbox;
 }
 
-std::tuple<int, int> Player::player_bottomright_collision_hitbox(){
+std::tuple<int, int> Player::Collision_Hitbox::player_bottomright_collision_hitbox(){
     std::tuple<int, int> player_topleft_hitbox = {getPosition_x() + 19, getPosition_y() + 32};    // edge INCLUDED
     return player_topleft_hitbox;
 }
 
-std::tuple<std::tuple<int, int>, std::tuple<int, int>> Player::collision_hitbox(){      // Returns (player_topleft_hitbox(), player_bottomright_hitbox())
+std::tuple<std::tuple<int, int>, std::tuple<int, int>> Player::Collision_Hitbox::collision_hitbox(){      // Returns (player_topleft_hitbox(), player_bottomright_hitbox())
     std::tuple<std::tuple<int, int>, std::tuple<int, int>> hitbox = {player_topleft_collision_hitbox(), player_bottomright_collision_hitbox()};
     return hitbox;
 }
 
-std::array<int, 4> Player::collision_hitbox_array(){
+std::array<int, 4> Player::Collision_Hitbox::collision_hitbox_array(){
 
-    std::array<int, 4> hitbox = {std::get<0>(Player::player_topleft_collision_hitbox()), std::get<1>(player_topleft_collision_hitbox()), std::get<0>(player_bottomright_damage_hitbox()), std::get<1>(player_bottomright_damage_hitbox())};
+    std::array<int, 4> hitbox = {std::get<0>(player_topleft_collision_hitbox()), std::get<1>(player_topleft_collision_hitbox()), std::get<0>(player_bottomright_damage_hitbox()), std::get<1>(player_bottomright_damage_hitbox())};
     return hitbox;
 }
 
-std::array<std::tuple<int, int>, 4> Player::collision_hitbox_vertex_array(){
+std::array<std::tuple<int, int>, 4> Player::Collision_Hitbox::collision_hitbox_vertex_array(){
 
-    std::tuple<int, int> topleft_corner = Player::player_topleft_collision_hitbox();
-    std::tuple<int, int> topright_corner = {std::get<0>(Player::player_bottomright_collision_hitbox()), std::get<1>(Player::player_topleft_collision_hitbox())};
-    std::tuple<int, int> bottomleft_corner = {std::get<0>(Player::player_topleft_collision_hitbox()), std::get<1>(Player::player_bottomright_collision_hitbox())};
-    std::tuple<int, int> bottomright_corner = Player::player_bottomright_collision_hitbox();
+    std::tuple<int, int> topleft_corner = Player::Collision_Hitbox::player_topleft_collision_hitbox();
+    std::tuple<int, int> topright_corner = {std::get<0>(Player::Collision_Hitbox::player_bottomright_collision_hitbox()), std::get<1>(Player::Collision_Hitbox::player_topleft_collision_hitbox())};
+    std::tuple<int, int> bottomleft_corner = {std::get<0>(Player::Collision_Hitbox::player_topleft_collision_hitbox()), std::get<1>(Player::Collision_Hitbox::player_bottomright_collision_hitbox())};
+    std::tuple<int, int> bottomright_corner = Player::Collision_Hitbox::player_bottomright_collision_hitbox();
 
     std::array<std::tuple<int, int>, 4> hitbox = {topleft_corner, topright_corner, bottomleft_corner, bottomright_corner};
     return hitbox;
