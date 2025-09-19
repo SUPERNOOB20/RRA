f13c57576488aa6ac3c0bba80e45ef3efc942f66
SUPERNOOB20 <andycia802@gmail.com>
2025-09-19 01:56:01 -0300

    refactorizes coordinates.cpp and coordinates.h - from tuples to arrays :3

--- source/include/coordinates.h
+++ source/include/coordinates.h
@@ -14,18 +14,18 @@ struct Rect_Coordinates{
     
     private:
         // Don't worry, it's just (x1, y1), (x2, y2)
-        std::tuple<int, int> top_left_corner;         
-        std::tuple<int, int> bottom_right_corner;     
+        std::array<int, 2> top_left_corner;         
+        std::array<int, 2> bottom_right_corner;     
 
     public:
-        Rect_Coordinates(std::tuple<int, int> top_left_corner, std::tuple<int, int> bottom_right_corner);
-        Rect_Coordinates(std::tuple<std::tuple<int, int>, std::tuple<int, int>> top_left_and_bottom_right_corners_inside_tuple);
-        Rect_Coordinates(std::tuple<int, int, int, int> int_coords);
+        Rect_Coordinates(std::array<int, 2> top_left_corner, std::array<int, 2> bottom_right_corner);
+        Rect_Coordinates(std::array<std::array<int, 2>, 2> top_left_and_bottom_right_corners_inside_tuple);
+        Rect_Coordinates(std::array<int, 4> int_coords);
 
-        std::tuple<int, int> get_top_left_corner(Rect_Coordinates coords);
+        std::array<int, 2> get_top_left_corner(Rect_Coordinates coords);
         int get_top_left_corner_x(Rect_Coordinates coords);
         int get_top_left_corner_y(Rect_Coordinates coords);
-        std::tuple<int, int> get_bottom_right_corner(Rect_Coordinates coords);
+        std::array<int, 2> get_bottom_right_corner(Rect_Coordinates coords);
         int get_bottom_right_corner_x(Rect_Coordinates coords);
         int get_bottom_right_corner_y(Rect_Coordinates coords);
 
--- source/src/coordinates.cpp
+++ source/src/coordinates.cpp
@@ -1,50 +1,53 @@
 #include "coordinates.h"
-#include <tuple>
+// #include <tuple>
+#include <array>
 
 
 
 
 
-Rect_Coordinates::Rect_Coordinates(std::tuple<int, int> top_left_corner, std::tuple<int, int> bottom_right_corner)
+Rect_Coordinates::Rect_Coordinates(std::array<int, 2> top_left_corner, std::array<int, 2> bottom_right_corner)
     : top_left_corner{ top_left_corner }
     , bottom_right_corner { bottom_right_corner }
 {
 }
 
-Rect_Coordinates::Rect_Coordinates(std::tuple<std::tuple<int, int>, std::tuple<int, int>> top_left_and_bottom_right_corners)
-    : top_left_corner{ std::get<0>(top_left_and_bottom_right_corners) }
-    , bottom_right_corner { std::get<1>(top_left_and_bottom_right_corners) }
+Rect_Coordinates::Rect_Coordinates(std::array<std::array<int, 2>, 2> top_left_and_bottom_right_corners)
+    : top_left_corner{ top_left_and_bottom_right_corners[0] }
+    , bottom_right_corner { top_left_and_bottom_right_corners[1] }
 {
 }
 
-Rect_Coordinates::Rect_Coordinates(std::tuple<int, int, int, int> int_coords)
-    : top_left_corner{ std::tuple<int, int> (std::get<0>(int_coords), std::get<1>(int_coords)) }
-    , bottom_right_corner{ std::tuple<int, int> (std::get<2>(int_coords), std::get<3>(int_coords)) }
+Rect_Coordinates::Rect_Coordinates(std::array<int, 4> int_coords)
+    : top_left_corner{ std::array<int, 2> {int_coords[0], int_coords[1]} }
+    , bottom_right_corner{ std::array<int, 2> {int_coords[2], int_coords[3])} }
 {
 }
 
 
 
-std::tuple<int, int> Rect_Coordinates::get_top_left_corner(Rect_Coordinates coords){
-    return top_left_corner;
+std::array<int, 2> Rect_Coordinates::get_top_left_corner(Rect_Coordinates coords){
+    return coords.top_left_corner;
 }
 
 int Rect_Coordinates::get_top_left_corner_x(Rect_Coordinates coords){
-    return std::get<0>(top_left_corner);
+    return coords.top_left_corner[0];
 }
 
 int Rect_Coordinates::get_top_left_corner_y(Rect_Coordinates coords){
-    return std::get<1>(top_left_corner);
+    return coords.top_left_corner[1];
 }
 
-std::tuple<int, int> Rect_Coordinates::get_bottom_right_corner(Rect_Coordinates coords){
-    return bottom_right_corner;
+std::array<int, 2> Rect_Coordinates::get_bottom_right_corner(Rect_Coordinates coords){
+    return coords.bottom_right_corner;
 }
 
 int Rect_Coordinates::get_bottom_right_corner_x(Rect_Coordinates coords){
-    return std::get<0>(bottom_right_corner);
+    // return std::get<0>(bottom_right_corner);
+    return coords.bottom_right_corner[0];
 }
 
 int Rect_Coordinates::get_bottom_right_corner_y(Rect_Coordinates coords){
-    return std::get<1>(bottom_right_corner);
+    // return std::get<1>(bottom_right_corner);
+    return coords.bottom_right_corner[1];
 }
